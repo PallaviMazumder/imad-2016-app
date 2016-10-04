f2d49614e4aa866bf75f3928f9852c5fe5a482f5
PallaviMazumder <pal.maz12342@gmail.com>
2016-10-04 20:21:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get(':/articleName',function (req,res) {
+app.get('/articleName',function (req,res) {
     //ArticleName == article-one
     //articles[articleName]== content object of articleOne
     var articleName = req.params.articleName;
