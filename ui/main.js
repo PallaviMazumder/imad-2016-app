a260dc6dcae17ec6c116a7d928a923ff962e1997
PallaviMazumder <pal.maz12342@gmail.com>
2016-10-11 19:20:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
+
+//move the image
+var img = document.getElementById('madi');
+madi.onclick = function(){
+    madi.style.marginLeft='100px';
+};
