445be7f6527023aa7068648316f0e0ee045be618
PallaviMazumder <pal.maz12342@gmail.com>
2016-10-11 22:41:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,15 @@
-console.log('Loaded!');
-//change the text of main-text div
-
-var element = document.getElementById('main-text');
-element.innerHTML = "New Value";
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft=marginLeft+'px';
-}
-madi.onclick = function(){
-    var interval = setInterval(moveRight,50);
-    madi.style.marginLeft='100px';
+//Counter code
+var button=document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    
+    //make a request to the counter endpoint
+    
+    //capture the response and store it in a varoable
+    
+    //render the variable in the correct span
+    counter = counter+1;
+    span=document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
 };
